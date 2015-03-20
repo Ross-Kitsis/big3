0b11040a15fcf837fb2b6dbdbcccb2ae665819ec
Ross-Kitsis <ross.kitsis@gmail.com>
2015-03-20 15:47:17 -0400

    First and second order matrix working

--- E.java
+++ E.java
@@ -10,7 +10,6 @@ public class E
 		int uniSize = 1;
 		int biSize = 2;
 		int triSize = 3;
-		int numWord = 1000;
 		Common c = new Common();
 		Map<String,Integer> unigrams = new HashMap<String,Integer>();
 		Map<String,Integer> bigrams = new HashMap<String,Integer>();
@@ -38,8 +37,16 @@ public class E
 					c.getNgrams(current, biSize, bigrams);
 					c.getNgrams(current, triSize, trigrams);
 				}
-			
-				buildFirstOrderMatrix(unigrams,files[i].getName());
+				
+				int pos = files[i].getName().indexOf('.');
+				String name = files[i].getName().substring(0,pos);
+				
+				//buildFirstOrderMatrix(unigrams,name);
+				buildSecondOrderMatrix(bigrams,name);
+				
+				unigrams.clear();
+				bigrams.clear();
+				trigrams.clear();
 				
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
@@ -54,7 +61,17 @@ public class E
 	}
 	public static void buildFirstOrderMatrix(Map<String,Integer> unigram, String title)
 	{
-		File f = new File("matrix\\" + title + ".csv");
+		char sep = File.separatorChar;
+		
+		String directory = "matrix"+sep+"FirstOrder"+sep;
+		File dir = new File(directory);
+		if(!dir.exists())
+		{
+			dir.mkdir();
+		}
+		
+		
+		File f = new File(directory + title + ".csv");
 		if(!f.exists())
 		{
 			try {
@@ -78,13 +95,79 @@ public class E
 		
 		PrintWriter pw;
 		try {
-			System.out.println(toWrite.toString());
+			//System.out.println(toWrite.toString());
 			
 			pw = new PrintWriter(f);
 			pw.write(toWrite + "\n");
 			pw.write(numGram + "\n");
 			pw.close();
 			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public static void buildSecondOrderMatrix(Map<String,Integer> bigrams, String title)
+	{
+		
+		char sep = File.separatorChar;
+		
+		String directory = "matrix"+sep+"SecondOrder"+sep;
+		File dir = new File(directory);
+		
+		if(!dir.exists())
+		{
+			dir.mkdir();
+		}
+		
+		
+		File f = new File(directory + title + ".csv");
+		if(!f.exists())
+		{
+			try {
+				f.createNewFile();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		String[] accepted = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r", "s"
+			,"t","u","v","w","x","y","z","#",",",".",";",":","?","!","(",")","-","\"","\'","@"," "};
+		
+		
+		try {
+			PrintWriter pw = new PrintWriter(f);
+			
+			String bi = "";
+			Integer numBi = 0;
+			String base= "\t";
+			for(int i = 0; i < accepted.length; i++)
+			{
+				base = base + accepted[i]+"\t";
+			}
+			pw.write(base+"\n");
+			
+			String toWrite = "";
+			for(int i = 0; i < accepted.length; i++)
+			{
+				toWrite = toWrite + accepted[i] + "\t";
+				for (int j = 0; j < accepted.length;j++)
+				{
+					bi = accepted[i]+accepted[j];
+					numBi = bigrams.get(bi);
+					if(numBi == null)
+					{
+						numBi = 0;
+					}
+					toWrite = toWrite + numBi + "\t";
+				}
+				pw.write(toWrite + "\n");
+				toWrite = "";
+			}
+			pw.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
